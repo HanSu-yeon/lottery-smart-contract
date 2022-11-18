760833ca83fc9756caaa6a4087f8d25732234480
HanSuyeon <js7122420@gmail.com>
2022-11-18 17:57:42 +0900

    Domain 및 Queue설계 중

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -1,6 +1,7 @@
 const _deploy_smart_contract = require("../migrations/1_deploy_smart_contract");
 const Lottery = artifacts.require("Lottery");
-
+//contract(테스트할 컨트랙트 명,callback function([deployer, user1,user2])) : 기본적인 모카 테스트 구조
+//파라미터 : account들이 순서대로 들어온다 지금은 0,1,2주소
 contract("Lottery", function ([deployer, user1, user2]) {
   let lottery;
   beforeEach(async () => {
@@ -14,5 +15,10 @@ contract("Lottery", function ([deployer, user1, user2]) {
     console.log(`owner : ${owner}`);
     console.log(`value : ${value}`);
     assert.equal(10, 10);
+  });
+  //모카에서 test할때 특정 테케만 테스트하고 싶으면 .only 써주기
+  it.only("getPot should return curdrent pot", async () => {
+    let pot = await lottery.getPot();
+    assert.equal(pot, 0);
   });
 });
